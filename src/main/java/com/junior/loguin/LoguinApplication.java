cfb44e92bbabb1a1ecbc3d8c8b537ee6f5da641e
JRicardoLeon <josericardobl1@outlook.com>
2022-06-01 07:51:43 -0500

    Creaion del proyecto y clonando lo con gitHub

---
+++ src/main/java/com/junior/loguin/LoguinApplication.java
@@ -0,0 +1,13 @@
+package com.junior.loguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoguinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junior/loguin/LoguinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.loguin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoguinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
